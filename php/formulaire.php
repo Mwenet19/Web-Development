444ab219644febd17a62e1a139b24ce9619ea191
jjean <jjeanlis@gmail.com>
2020-12-10 21:23:08 -0700

    template

--- php/formulaire.php
+++ php/formulaire.php
@@ -3,19 +3,19 @@
 
 <?php include 'db_connect.php'; 
 
-$mysqli = new mysqli("localhost", $username, $password, $database);
+//$mysqli = new mysqli($localhost, $username, $password, $database);
 
 // Don't forget to properly escape your values before you send them to DB
 // to prevent SQL injection attacks.
 
-$nom = $mysqli->real_escape_string($_POST[$nom]);
-$prenom = $mysqli->real_escape_string($_POST[$prenom]);
-$pays = $mysqli->real_escape_string($_POST[$pays]);
-$langue = $mysqli->real_escape_string($_POST[$langue]);
-$montant = $mysqli->real_escape_string($_POST[$montant]);
+$nom = $mysqli->real_escape_string($_POST[nom]);
+$prenom = $mysqli->real_escape_string($_POST[prenom]);
+$pays = $mysqli->real_escape_string($_POST[pays]);
+$langue = $mysqli->real_escape_string($_POST[langue]);
+$montant = $mysqli->real_escape_string($_POST[montant]);
 
 $query = "INSERT INTO formulaire (nom, prenom, pays, langue, montant)
-            VALUES ('Paul','Lubin','Guatemal','French','50.6')";
+            VALUES ('Paul','Lubin','Guatemalg','French',50.6)";
 
 $mysqli->query($query);
 $mysqli->close();
